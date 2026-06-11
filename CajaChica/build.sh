787988e55eddefad14c6ee344df0ccf95feb3036
ahruiz <ahruiz0@gmail.com>
2026-06-11 01:36:37 -0700

    Fix: conf de postgreSQL, env en render y superusr

--- CajaChica/build.sh
+++ CajaChica/build.sh
@@ -10,3 +10,5 @@ python manage.py collectstatic --no-input
 
 # Aplicar migraciones a PostgreSQL
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@email.com', 'admin123')"
